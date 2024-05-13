f20749b1abffb343d2e3e23b6f515f650c39e772
FALKed <92156244+FALKed@users.noreply.github.com>
2024-05-13 12:48:10 +0200

    Update ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,7 +1,34 @@
 from pytube import YouTube
+from pytube import Playlist
 from sys import argv
 
+# get link via argument 
+# TODO: check if argument is empty: print error message "a link to video/playlist needs to be provided"
 link = argv[1]
-yt = YouTube(link)
 
-yd = yt.streams.get_
+
+# TODO: check if provided link actually links to a YT playlist or video
+def validateLink(link):
+    if(1):
+        return True
+    return False
+
+# TODO: check automatically if link provided is a playlist or a video and proceed accordingly
+def detectVideoOrPlaylist(link):
+    print("Hello World")
+
+# download single video
+def downloadVideo(videoLink):
+    video = YouTube(videoLink)
+    print("Downloading Video: ", video.title)
+    downloader = video.streams.get_highest_resolution()
+    downloader.download('/Users/bastiangerkum/Desktop')
+
+def downloadPlaylist(playlistLink):
+    print("Playlist")
+    playlist = Playlist(playlistLink)
+    plTitle = playlist.title
+    print("Downloading Playlist: ", plTitle)
+    #TODO: depending on OS create folder named after plTitle (if doesnt exist already)
+    for videoURL in playlist.video_urls:
+        downloadVideo()
