ce2cd241fd51cc8b3665760180e9c4d3ec3dacaa
FALKed <92156244+FALKed@users.noreply.github.com>
2024-05-13 13:52:42 +0200

    Update ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,6 +1,26 @@
 from pytube import YouTube
 from pytube import Playlist
 from sys import argv
+import os
+
+# check which OS the script is being run on
+def isWindows():
+    if os.name == "nt":
+        return True
+    return False
+
+# depending on OS select Downloadpath
+def getDownloadPath():
+    homePath = os.path.expanduser("~")
+    desktopPath = os.path.join(homePath, "Desktop")
+    downloadFolderPath = os.path.join(desktopPath, "ytDownloads")
+    if not os.path.exists(downloadFolderPath):
+        os.makedirs(downloadFolderPath)
+        print(f"Folder #{downloadFolderPath} created successfully")
+    return downloadFolderPath
+
+def makePlaylistFolder(playlistName):
+    print("testFolder");
 
 # TODO: check if provided link actually links to a YT playlist or video
 def validateLink(link):
@@ -13,26 +33,39 @@ def detectVideoOrPlaylist(link):
     print("Hello World")
 
 # download single video
-def downloadVideo(videoLink):
+def downloadVideo(videoLink, playlistFolder=None):
+    print("test2")
+    downloadPath = getDownloadPath()
+    if playlistFolder is not None:
+        downloadPath = os.path.join(downloadPath, playlistFolder)
+        if not os.path.exists(downloadPath):
+            os.makedirs(downloadPath)
+            print(f"Folder #{downloadPath} created successfully")
     video = YouTube(videoLink)
     print("Downloading Video: ", video.title)
     downloader = video.streams.get_highest_resolution()
-    downloader.download('/Users/bastiangerkum/Desktop')
+    downloader.download(downloadPath)
 
+# download all videos in a playlist
 def downloadPlaylist(playlistLink):
     print("Playlist")
     playlist = Playlist(playlistLink)
     plTitle = playlist.title
+    plLength = playlist.length
     print("Downloading Playlist: ", plTitle)
+    print(f"Playlist contains of {plLength} videos")
     #TODO: depending on OS create folder named after plTitle (if doesnt exist already)
-    plLength = playlist.length-1
+    
     videoCounter = 0
-    for videoURL in playlist.video_urls:
-        downloadVideo(videoURL)
+    print("test")
+    for videourl in playlist.video_urls[:3]:
+        print("videourl")
+        downloadVideo(videourl, plTitle)
         videoCounter+=1
         print("Progress: ", videoCounter, "/", plLength, " Videos")
+
 
 # get link via argument 
 # TODO: check if argument is empty: print error message "a link to video/playlist needs to be provided"
 link = argv[1]
-downloadVideo(link)
+downloadPlaylist(link)
